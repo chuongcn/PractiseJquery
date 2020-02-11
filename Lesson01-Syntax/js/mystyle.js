2132cb17c0ad2122d4a7692cea1357a9cc0dea72
chuongcn <chuongchung.it@gmail.com>
2020-02-11 12:53:39 +0700

    Lesson01 - Syntax Jquery

---
+++ Lesson01-Syntax/js/mystyle.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Thay đổi dữ liệu cho đối tượng nào => p {selector}  
+    // Thay đổi cái gì? Text (method text jquery)
+
+    $("p span.first-name").text("Chung");
+    $("p span.last-name").text("Nghiep Chuong");
+
+});
