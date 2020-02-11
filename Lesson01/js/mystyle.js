fc61d32d7d81ea96310edeb4e22527f9b11bf11b
chuongcn <chuongchung.it@gmail.com>
2020-02-11 13:28:23 +0700

    Learn Jquery Unitop 2020

--- Lesson01/js/mystyle.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.ndmotkhoi').slideUp();
-
-    $('.motkhoi h3').click(function (event) {
-        console.log('Đã click');
-        $('.ndmotkhoi').slideDown();
-    });
-});
